2ee4707dab52b75e0a0ea0b29df0445bf7a02c63
ajimeno04 <jimenobeltran11@gmail.com>
2024-10-22 14:55:08 +0200

    feat: add kakarot sepolia network

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -853,6 +853,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 203994162, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 203994202, "1.3.0"),
     ],
+    EthereumNetwork.KAKAROT: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 239183, "1.3.0"),
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 239180, "1.3.0+L2"),
+    ],
     EthereumNetwork.SCROLL_SEPOLIA_TESTNET: [
         (
             "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
@@ -2062,6 +2066,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.METIS_STARDUST_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 56117),  # v1.3.0
+    ],
+    EthereumNetwork.KAKAROT: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",  239112),  # v1.3.0
     ],
     EthereumNetwork.METIS_GOERLI_TESTNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 131842),  # v1.3.0
